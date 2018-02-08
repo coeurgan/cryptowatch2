0c06b4c089fd640c3fff4db22846cdade6dbe3fb
coeurgan <coeurgan@gmail.com>
2018-02-08 16:24:37 +0100

    backup

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BaseRequestOptions, Http} from '@angular/http';
 import { Coin } from './coin';
 import { COINS } from './mock-coins';
 
@@ -10,11 +11,26 @@ import { COINS } from './mock-coins';
 export class CoinsComponent implements OnInit {
 
   coins=COINS;
+	
+
+
+
   
-  
-    constructor() { }
+  constructor(private Http: Http) { }
 
   ngOnInit() {
+	for (let i = 0; i < this.coins.length; i++) {
+		var codes = "";
+		var coin = this.coins[i];
+		codes = codes + coin.code + ",";
+	}
+	var data;
+	  var url = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms="+codes+"&tsyms=USD";
+	 http.get(url).subscribe((res:Response) => data = res.json());
+	for (number i = 0; i < this.coins.length; i++) {
+		var coin = coins[i];
+		coin.price = data.RAW[coin.code].USD.PRICE;
+	}
   }
 
 }
